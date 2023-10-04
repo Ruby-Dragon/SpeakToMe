3a8714abc626735e1022d04f4c52d7bd2df99cc3
Ruby-Dragon <91497228+Ruby-Dragon@users.noreply.github.com>
2023-10-04 10:50:20 -0700

    add tutorial and license

--- main.cpp
+++ main.cpp
@@ -7,6 +7,62 @@
 #define ROW_MIN 12
 #define COLUMN_MIN 20
 
+void PrintTitleScreen()
+{
+    int row, col;
+    getyx(stdscr, row, col);
+
+    move(row, 0);
+    addstr("Speak To Me");
+    move(row+1, 0);
+    addstr("Type \"a\" to Start");
+    move(row+2, 0);
+    addstr("Type \"b\" for Tutorial");
+    move (row+3, 0);
+    addstr(":/");
+}
+
+void Tutorial(char Input)
+{
+    if (Input != 'b')
+    {
+        return;
+    }
+
+    clear();
+
+    int row, col;
+    move(0,0);
+    getyx(stdscr, row, col);
+
+    char Answer;
+
+    addstr("This game is played by reading dialogue, and picking a response.");
+    move(row+1, row);
+    addstr("You will be conversing with an... intelligence. Remember, it has feelings too!");
+    move(row+2, row);
+    addstr("There will be responses labeled with letters, type the letter of the response you would like to say!");
+    move(row+3, row);
+    addstr("Do you understand?");
+    move(row+4, row);
+    addstr("a. Yes!");
+    move(row+5, row);
+    addstr("b. No!");
+    move(row+6, row);
+    addstr(":/");
+    scanw("%c", &Answer);
+
+    if (Answer == 'a')
+    {
+        return;
+    }
+
+    clear();
+    move(0,0);
+    addstr("Well that is too bad, the game will begin anyway once you press any key.");
+    getch();
+}
+
 int main()
 {
     initscr();
@@ -17,11 +73,8 @@ int main()
 
     //not used yet, but could be used for more interesting effects later...
     bool Playing = true;
-
     char Input;
-
     AI TheAI = AI();
-
     int row, col;
 
     getmaxyx(stdscr, row, col);
@@ -39,6 +92,12 @@ int main()
     //god knows why this has to be here
     scanw("%c", &Input);
 
+    PrintTitleScreen();
+    scanw("%c", &Input);
+    Tutorial(Input);
+
+    clear();
+
     while (Playing)
     {
         move(0,0);
@@ -55,7 +114,7 @@ int main()
         getyx(stdscr, row, col);
 
         move(row + 1,0);
-        addstr("Select Response: ");
+        addstr(":/");
         scanw("%c", &Input);
 
         std::string InputString = std::string();
@@ -71,7 +130,10 @@ int main()
         {
             getyx(stdscr, row, col);
             move(row + 1,0);
-            addstr("Please enter a valid option: ");
+            addstr("Please enter a valid option.");
+            getyx(stdscr, row, col);
+            move(row + 1,0);
+            addstr(":/");
             scanw("%c", &Input);
 
             InputString[0] = Input;
